9e0f504803ffb9ba8aedcbb7f5b68b7d68667b3a
tmp <tmp@DESKTOP-26R79EV>
2025-11-05 16:55:32 +0100

    Habe die funktion move() eingefung jetzt kann man die figur bewegen;

--- Kleines_Game/Kleines_Game.cpp
+++ Kleines_Game/Kleines_Game.cpp
@@ -12,19 +12,27 @@ using namespace ::std;
 class Feld {
 
 public:
-	int grosse[5][5] = {0}
-	void setPos(int a, int b)
-	{
+	//int grosse[5][5] = { 0 };
+	//void newPos(int a, int b) {
+	//	pos[0] = a;
+	//	pos[1] = b;
+	
+	//int getPos() {
+	//	/*int positionH = pos[0];
+	//	int poistionB = pos[1];*/
 
-	}
+	//}
 
 };
+
+
 class Monster {
 
 public:
 	int HP = 100;
 	int AT = 20;
 	int rang = 1;
+	int pos[2] = { 1,1 };
 
 	void getStatus()
 	{
@@ -41,23 +49,30 @@ class Mensch {
 public:
 	int HP = 30;
 	int AT = 10;
-	int posMensch[2] = { 1,1 };
+	int pos[2] = { 1, 1 };
 	int rang = 1;
 	int hoche = 1;
 	int breite = 1;
 
-	void newPos(int a, int c) {
-		posMensch[2] = { a, c };
-	}
-	int getPos() {
-		return hoche;
-		return breite;
+	void move(int dx, int dy) {
+		int neuX = pos[0] + dx;
+		int neuY = pos[1] + dy;
 
+		if (neuX >= 0 && neuX < 5 && neuY >= 0 && neuY < 5) {
+			pos[0] = neuX;
+			pos[1] = neuY;
+			cout << "Bewegt nach [" << pos[0] << "][" << pos[1] << "]" << endl;
+		}
+		else {
+			cout << "bewegung auserhalb spielfeld";
+		}
 	}
+
+	
 	void getStatus()
 	{
 		cout << "Dein HP ist: " << HP << endl;
-
+		
 	}
 	int Atake(int a)
 	{
@@ -69,12 +84,13 @@ using namespace std;
 
 int main()
 {
-	Feld feld;
+	
 	Mensch a;
 	Monster b;
 	b.HP = a.Atake(b.HP);
 	b.getStatus();
-	cout<<a.getPos();
+	a.move(1, 3);
+	
 	
 
 
